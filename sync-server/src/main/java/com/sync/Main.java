ff15f0178157d731ca431275a3583081e44b4041
rhysemmas <rhys.emmas@neo4j.com>
2024-11-25 22:47:06 +0000

    Hello world spark http server

--- sync-server/src/main/java/com/sync/Main.java
+++ sync-server/src/main/java/com/sync/Main.java
@@ -1,7 +1,41 @@
 package com.sync;
 
+import org.apache.logging.log4j.Level;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Service;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Logger logger = LogManager.getLogger();
+        var ipAddress = "127.0.0.1";
+        var port = 8080;
+
+        Service service = Service.ignite();
+        service.ipAddress(ipAddress).port(port);
+
+        MyRoute route = new MyRoute(logger);
+        service.get("/", "*/*", route);
+
+        service.awaitInitialization();
+        logger.info("Http server started {}:{}", ipAddress, port);
+    }
+}
+
+class MyRoute implements Route {
+    private final Logger logger;
+
+    MyRoute(Logger logger) {
+        this.logger = logger;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        logger.log(Level.INFO, "Got request from: {}", request.ip());
+        return "wow\n";
     }
 }
